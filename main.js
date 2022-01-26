ce5b0a625a6596aef47ab63b357137db885ef302
lhk3337 <lhk3337@naver.com>
2022-01-27 00:54:43 +0900

    arrow up with JS

--- main.js
+++ main.js
@@ -11,15 +11,28 @@ document.addEventListener("scroll", () => {
 });
 
 const home = document.querySelector(".home__container");
+const arrowUpBtn = document.querySelector(".arrow__up");
 const homeHeight = home.getBoundingClientRect().height;
 
 document.addEventListener("scroll", () => {
-  // console.log("window.scrollY", window.scrollY);
-  // console.log("homeHeight", homeHeight);
-  // console.log(window.scrollY / homeHeight);
+  //  console.log("window.scrollY", window.scrollY);
+  //   console.log("homeHeight", homeHeight - 200);
+  //   console.log("opacity", window.scrollY / homeHeight);
   if (window.scrollY < homeHeight) {
     home.style.opacity = 1 - window.scrollY / homeHeight;
   }
+});
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUpBtn.classList.add("visible");
+  } else {
+    arrowUpBtn.classList.remove("visible");
+  }
+});
+
+arrowUpBtn.addEventListener("click", () => {
+  document.querySelector("#home").scrollIntoView({ behavior: "smooth" });
 });
 
 const navMenuBtn = document.querySelectorAll(".navbar__menu__item");
